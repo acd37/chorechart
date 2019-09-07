b81398f6ce7ec25ceb38e875d68569ea67095a7c
Alec Down <alecdown@gmail.com>
2019-09-07 17:50:24 -0500

    initial commit

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
+const config = require(path.join(__dirname, '..', 'config', 'config.js'))[env];
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 const db = {};
