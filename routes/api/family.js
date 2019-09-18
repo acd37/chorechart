20d1cad9a137c0ce35c56cd1a6c103e736f8b332
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-18 18:37:36 -0500

    formatting

--- routes/api/family.js
+++ routes/api/family.js
@@ -14,69 +14,14 @@ module.exports = function(app) {
     // @route GET api/family/
     // @desc gets all families
     app.get('/api/family/', passport.authenticate('jwt', { session: false }), (req, res) => {
-        db.family.findAll({}).then((family) => {
-            res.status(200).json(family);
-        }).catch((err) => {
-            res.status(500).json(err)
-        })
-    })
-
-    // @route POST api/users/
-    // @desc creates new family
-    // adds family ID to user familyId
-    app.post('/api/family', passport.authenticate('jwt', { session: false }), (req, res) => {
-
-        db.user.findOne({
-            where: {
-                id: req.user.id
-            }
-        }).then((user) => {
-            if (user.familyId) {
-                return res.status(400).json({ familyId: 'You are already a member of a family.' });
-            } else {
-                let familyId = "";
-                db.family.create(req.body).then((family) => {
-                    familyId = family.id;
-                    db.user.update(
-                        {
-                            familyId: family.id
-                        },
-                        {
-                            where: {
-                                id: user.id
-                            }
-                        }
-                    ).then((user) => {
-                        res.status(201).json({
-                            // For API Testing
-                            // NOTE: Remove prior to release.
-                            familyId: familyId,
-                            message: "Family successfully created.",
-                            familyCreated: true
-                        }); 
-                    }).catch((err) => {
-                        res.status(500).json(err);
-                    })
-                })
-            }
-        })
-    });
-
-    // @route DELETE api/users/:id
-    // @desc deletes family
-    app.delete('/api/family/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
-        db.family.destroy({
-            where: {
-                id: req.params.id
-            }
-        }).then(() => {
-            res.status(200).json({
-                message: "Family successfully deleted.",
-                familyDeleted: true
+        db.family
+            .findAll({})
+            .then((family) => {
+                res.status(200).json(family);
             })
-        }).catch((err) => {
-            res.status(500).json(err);
-        })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
     });
 
     // @route GET api/family/current
@@ -91,6 +36,73 @@ module.exports = function(app) {
             })
             .then((family) => res.status(200).json(family))
             .catch((err) => console.log(err));
+    });
+
+    // @route POST api/users/
+    // @desc creates new family
+    // adds family ID to user familyId
+    app.post('/api/family', passport.authenticate('jwt', { session: false }), (req, res) => {
+        db.user
+            .findOne({
+                where: {
+                    id: req.user.id
+                }
+            })
+            .then((user) => {
+                if (user.familyId) {
+                    return res
+                        .status(400)
+                        .json({ familyId: 'You are already a member of a family.' });
+                } else {
+                    let familyId = '';
+                    db.family.create(req.body).then((family) => {
+                        familyId = family.id;
+                        db.user
+                            .update(
+                                {
+                                    familyId: family.id
+                                },
+                                {
+                                    where: {
+                                        id: user.id
+                                    }
+                                }
+                            )
+                            .then((user) => {
+                                res.status(201).json({
+                                    // For API Testing
+                                    // NOTE: Remove prior to release.
+                                    familyId: familyId,
+                                    message: 'Family successfully created.',
+                                    familyCreated: true
+                                });
+                            })
+                            .catch((err) => {
+                                res.status(500).json(err);
+                            });
+                    });
+                }
+            });
+    });
+
+    // @route DELETE api/users/:id
+    // @desc deletes family
+    app.delete('/api/family/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+        db.family
+            .destroy({
+                where: {
+                    id: req.params.id
+                }
+            })
+            .then(() => {
+                res.status(200).json({
+                    message: 'Family successfully deleted.',
+                    familyDeleted: true
+                });
+            })
+            .catch((err) => {
+                res.status(500).json(err);
+            });
     });
 
     // @route PUT api/family/:id
