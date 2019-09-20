725e3021e0010680fbf88eff782a146b99fef709
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-20 09:39:51 -0500

    adds GET to chore API to retrieve all chores based on familyId

--- routes/api/chore.js
+++ routes/api/chore.js
@@ -11,6 +11,20 @@ module.exports = function(app) {
         })
     })
 
+    // @route GET api/chore
+    // @desc get all chores for a given familyId
+    app.get('/api/chore', passport.authenticate('jwt', { session: false }), (req, res) => {
+        db.chore.findAll({
+            where: {
+                familyId: req.user.familyId
+            }
+        }).then((chores) => {
+            res.status(200).json(chores);
+        }).catch((err) => {
+            res.status(500).json(err);
+        })
+    })
+
     // @route POST api/users/test
     // @desc creates chore
     app.post('/api/chore', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -21,8 +35,47 @@ module.exports = function(app) {
         }).then((chore) => {
             res.json({
                 data: chore,
-                success: true,
+                choreCreated: true,
                 msg: 'Chore successfully created.'
+            })
+        }).catch((err) => {
+            res.status(500).json(err);
+        })
+    })
+
+    // @route PUT api/chore/:id
+    // @desc updates a chore
+    app.put('/api/chore/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+        db.chore.update({
+            description: req.body.description,
+            pointValue: req.body.pointValue,
+            isComplete: req.body.isComplete,
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }).then((chore) => {
+            res.json({
+                choreUpdated: true,
+                msg: 'Chore successfully updated.'
+            })
+        }).catch((err) => {
+            res.status(500).json(err);
+        })
+    })
+
+    // @route DELETE api/chore/:id
+    // @desc deletes a chore
+    app.delete('/api/chore/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+        db.chore.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then((chore) => {
+            res.json({
+                choreDeleted: true,
+                msg: 'Chore successfully deleted.'
             })
         }).catch((err) => {
             res.status(500).json(err);
