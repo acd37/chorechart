3f0079277b3ba9f926301e7470d43deeeff0d4ee
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-20 08:34:27 -0500

    adds POST api to chore route

--- routes/api/chore.js
+++ routes/api/chore.js
@@ -2,13 +2,32 @@ module.exports = function(app) {
     const db = require('../../models');
     const passport = require('passport');
 
-    // @route GET api/users/test
-    // @desc tests the users api route
+    // @route GET api/chore/test
+    // @desc tests the chore api route
     app.get('/api/chore/test', (req, res) => {
         res.json({
             success: true,
             msg: 'Testing endpoint works correctly.'
         })
     })
+
+    // @route POST api/users/test
+    // @desc creates chore
+    app.post('/api/chore', passport.authenticate('jwt', { session: false }), (req, res) => {
+        db.chore.create({
+            description: req.body.description,
+            pointValue: req.body.pointValue,
+            familyId: req.user.familyId
+        }).then((chore) => {
+            res.json({
+                data: chore,
+                success: true,
+                msg: 'Chore successfully created.'
+            })
+        }).catch((err) => {
+            res.status(500).json(err);
+        })
+    })
+
 
 }
