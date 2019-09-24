95ce9704f770c29c6c8f466a5c43bc486ce37bb1
Alec Down <alecdown@gmail.com>
2019-09-24 08:51:14 -0500

    removes function from Login component as props

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/pages/Login';
-import Dashboard from './components/pages/Dashboard';
+import Register from './components/pages/Register';
 
 class App extends Component {
-    handleChange = (e) => {
-        const { name, value } = e.target;
-
-        this.setState({
-            [name]: value
-        });
-    };
-
     render() {
         return (
             <div className='App'>
                 <h1> ChoreChart </h1>
-                <Login handleLogin={this.handleLogin} />
+                <Login />
             </div>
         );
     }
