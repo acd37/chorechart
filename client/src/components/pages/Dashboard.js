6523f35437ea5fe9d5e2d9fa05205e6deeade725
Alec Down <alecdown@gmail.com>
2019-10-20 16:03:26 -0500

    prints to page

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Dashboard extends Component {
+    state = {
+        user: {}
+    };
+
+    componentDidMount() {
+        const id = this.props.location.state.userID;
+
+        axios
+            .get('/api/user/current')
+            .then((res) => {
+                this.setState({
+                    user: res.data
+                });
+            })
+            .catch((err) => console.log(err.response.data));
+    }
+
     render() {
         return (
             <div>
                 <h1>Dashboard</h1>
+                <div>{JSON.stringify(this.state.user)}</div>
             </div>
         );
     }
